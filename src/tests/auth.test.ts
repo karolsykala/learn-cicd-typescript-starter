04cd6c1bcad7f86d774528fa8daa98ff4887bb74
Mlem <karol.sykala@protonmail.com>
2025-07-13 22:52:45 +0200

    add tests

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+  isActive: 0,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
